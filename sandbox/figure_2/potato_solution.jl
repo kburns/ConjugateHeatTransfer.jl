a62f168ff0b902f6a5e76b1d572e52f048d1a7cf
Keaton J. Burns <keaton.burns@gmail.com>
2024-09-05 14:37:15 -0400

    Update fig2 plots

--- sandbox/figure_2/potato_solution.jl
+++ sandbox/figure_2/potato_solution.jl
@@ -72,23 +72,59 @@ for body in bodies
     T[interior] .= NaN
 end
 
+
+# Evaluate in w domain
+gridsize = 200
+φw = collect(LinRange(-5.1, 5.1, gridsize));
+ψw = collect(LinRange(-5.1, 5.1, gridsize));
+ww = φw' .+ im*ψw;
+Tw = EvaluateSystem(real(ww), imag(ww), panels; quadrature=plot_quad);
+
+
 # Plot
 fig = Figure(size=(700, 300));
 set_theme!(theme_latexfonts());
 θp = LinRange(0, 2π, 1000);
 
-ax = Axis(fig[1,1], aspect=DataAspect(), title=L"Streamfunction $\psi$");
-co = contourf!(ax, x, y, ψ', levels=20);
-for body in bodies
-    boundary = body.zθ.(θp)
-    lines!(ax, real(boundary), imag(boundary), color=:black, linewidth=4)
-end
-Colorbar(fig[1,2], co);
+# ax = Axis(fig[1,1], aspect=DataAspect(), title=L"Streamfunction $\psi(z)$");
+# co = contourf!(ax, x, y, ψ', levels=20);
+# for body in bodies
+#     boundary = body.zθ.(θp)
+#     lines!(ax, real(boundary), imag(boundary), color=:black, linewidth=4)
+# end
+# Colorbar(fig[1,2], co);
+T_levels = LinRange(0, 1.5, 20);
 
-ax = Axis(fig[1,3], aspect=DataAspect(), title=L"Temperature $T$");
-poly!(ax, [(-3, -3), (3, -3), (3, 3), (-3, 3)], color=:gray, rasterize=true)
-co = contourf!(ax, x, y, T', levels=20, nan_color=:black);
+ax = Axis(fig[1,1], aspect=DataAspect(), title=L"Temperature $T(w)$");
+co = contourf!(ax, φw, ψw, Tw', levels=T_levels, nan_color=:black);
+# contour!(ax, φw, ψw, real(ww), levels=ψ_levels, color=(:white,0.75), linewidth=0.5);
 colorrange = (minimum(co._computed_levels.val), maximum(co._computed_levels.val));
+for body in bodies
+    lines!(ax, [body.φmin, body.φmax], [0, 0], color=:black, linewidth=2)
+    # φ0 = (body.φmin + body.φmax) / 2
+    # s = (body.φmax - body.φmin) / 2
+    # boundary = φ0 .+ s * cos.(θp) .+ 0.05im*((θp .< π) - (θp .> π))
+    # temp = body.Tη.(θp)
+    # #poly!(ax, [(real(p), imag(p)) for p in boundary], color=:gray)
+    # lines!(ax, real(boundary), imag(boundary), color=:black, linewidth=2, colorrange=colorrange)
+    # lines!(ax, real(boundary), imag(boundary), color=temp, linewidth=1, colorrange=colorrange, smooth=true)
+    # # Replot with shift to get rid of color artifacts
+    # boundary = φ0 .+ s * cos.(θp .+ π/1000) .+ 0.1im*((θp .< π) - (θp .> π))
+    # temp = body.Tη.(θp.+π/1000)
+    # lines!(ax, real(boundary), imag(boundary), color=temp, linewidth=1, colorrange=colorrange, smooth=true)
+end
+xlims!(ax, -5.1, 5.1)
+ylims!(ax, -5.1, 5.1)
+# Colorbar(fig[1,2], co);
+
+ax = Axis(fig[1,2], aspect=DataAspect(), title=L"Temperature $T(z)$");
+poly!(ax, [(-3, -3), (3, -3), (3, 3), (-3, 3)], color=:gray, rasterize=true)
+co = contourf!(ax, x, y, T', levels=T_levels, nan_color=:black);
+colorrange = (minimum(co._computed_levels.val), maximum(co._computed_levels.val));
+
+ψ_levels = collect(LinRange(-3, 3, 13)) .+ bodies[1].ψ
+contour!(ax, x, y, ψ', levels=ψ_levels, color=(:white,0.75), linewidth=0.5);
+
 for body in bodies
     boundary = body.zθ.(θp)
     temp = body.Tθ.(θp)
@@ -100,7 +136,10 @@ for body in bodies
     temp = body.Tθ.(θp.+π/1000)
     lines!(ax, real(boundary), imag(boundary), color=temp, linewidth=2, colorrange=colorrange, smooth=true)
 end
-Colorbar(fig[1,4], co);
+xlims!(ax, -3.1, 3.1)
+ylims!(ax, -3.1, 3.1)
+
+Colorbar(fig[1,3], co);
 
 save("sandbox/figure_2/potato.pdf", fig);
 
